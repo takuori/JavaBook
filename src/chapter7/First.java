da72058f871d11245a6957b965af4a8b9432ddbb
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-09-14 11:24:54 +0900

    [13thCommit]継承関係(コンストラクタの連鎖)

---
+++ src/chapter7/First.java
@@ -0,0 +1,14 @@
+package chapter7;
+
+class First {
+	public First() {
+		System.out.print("First");
+	}
+	
+}
+
+class Second extends First {
+	public Second() {
+		System.out.print("Second");
+	}
+}
---
+++ src/chapter7/Pra.java
@@ -0,0 +1,9 @@
+package chapter7;
+
+public class Pra {
+	public static void main(String[] args) {
+		Second s = new Second();
+		First f = new First();
+	}
+
+}
