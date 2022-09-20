908de25c49c26c5685ceec0add3f6f1485932536
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-09-20 11:47:58 +0900

    [28thCommit]ch6Memberクラスにequals()とhashcode()の自動生成実践

--- src/chapter6/Member.java
+++ src/chapter6/Member.java
@@ -1,5 +1,7 @@
 package chapter6;
 
+import java.util.Objects;
+
 public class Member {
 	private int id;
 	private String name;
@@ -28,6 +30,23 @@ public class Member {
 	@Override
 	public String toString() {
 		return "Member [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		return id == other.id;
 	}
 	
 	
