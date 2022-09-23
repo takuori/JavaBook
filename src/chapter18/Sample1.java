29831e310f451eeee2c14e2d3269d39ff0c7349e
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-09-23 10:24:25 +0900

    [33thCommit]保存漏れ、再コミット

--- src/chapter18/Sample1.java
+++ src/chapter18/Sample1.java
@@ -10,8 +10,10 @@ public class Sample1 {
 			c_list.add(new Child());
 			c_list.add(new Child());
 			
+			//Parent型の不変リストをChild型のリストから作成している（境界ワイルドカード型<? extends T>サブクラス型への拡張)
 			List<Parent> p_list = List.copyOf(c_list);
 			
+			//Child型のリストを並び替えるため、Parentクラスのフィールドを指定している。（境界ワイルドカード型<? super E>スーパークラス型への拡張）
 			c_list.sort(Comparator.comparing(Parent::key));
 		
 	}
