cbde76a9e9ff0be4f25fc16cf1d41fb2e3d4333a
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-09-27 15:11:24 +0900

    [46thCommit]文字列について練習場

---
+++ src/chapter23/Sample.java
@@ -0,0 +1,21 @@
+package chapter23;
+
+public class Sample {
+	public static void main(String[] args) {
+		//テキストブロック（二重引用符）
+		String str = """
+				玉井詩織 \
+				百田夏菜子 \
+				真田丸 \
+				""";
+		System.out.println(str);
+		
+		String str1 = """
+				name:%s
+				age:%d
+				weight:%.1f
+				""".formatted("玉井詩織", 27, 158.4);
+		System.out.println(str1);
+	}
+
+}
